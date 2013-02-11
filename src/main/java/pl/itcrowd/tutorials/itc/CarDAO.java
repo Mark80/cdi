059a62f11ffb7d366831757f21f8bfbb69b8cbb5
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2013-02-11 11:09:02 +0100

    Dependant stateless EJB

--- src/main/java/pl/itcrowd/tutorials/itc/CarDAO.java
+++ src/main/java/pl/itcrowd/tutorials/itc/CarDAO.java
@@ -1,9 +1,11 @@
 package pl.itcrowd.tutorials.itc;
 
+import javax.ejb.Stateless;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+@Stateless
 public class CarDAO implements Serializable {
 
     private List<Car> cars = new ArrayList<Car>();
